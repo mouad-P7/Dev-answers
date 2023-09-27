d194a5ceaa3415c8d3392dd5e7604c15909f4a88
mouad-p7 <mouadananouch7@gmail.com>
2023-09-27 16:45:47 +0100

    Create (auth) and (root) route groupes.

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex-center flex-col">
-      <h1 className="h1-bold">test h1-bold</h1>
-      <h2 className="h2-bold">test h2-bold</h2>
-      <h3 className="h3-bold">test h3-bold</h3>
-    </main>
-  );
-}
