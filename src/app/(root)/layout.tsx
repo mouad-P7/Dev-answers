b5c29c369cb52b7e46fcb4b5f74dde72e32de18c
mouad-p7 <mouadananouch7@gmail.com>
2023-09-28 14:25:48 +0100

    Create the structure of <Navbar/>.

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Navbar from "@ui/shared/Navbar";
 
 export default function RootLayout({
   children,
@@ -7,15 +8,15 @@ export default function RootLayout({
 }) {
   return (
     <div className="background-light850_dark100 relative">
-      {/* <Navbar /> */}
+      <Navbar />
       <div className="flex">
-        {/* <LeftSideBar /> */}
+        LeftSideBar
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
           <div className="mx-auto w-full max-w-5xl">{children}</div>
         </section>
-        {/* <RightSideBar /> */}
+        RightSideBar
       </div>
-      {/* <Toaster /> */}
+      Toaster
     </div>
   );
 }
---
+++ src/components/shared/Navbar.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import Image from "next/image";
+import { UserButton, SignedIn } from "@clerk/nextjs";
+
+export default function Navbar() {
+  return (
+    <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
+      <Link href="/" className="flex-center gap-1">
+        <Image
+          src="/assets/images/site-logo.svg"
+          alt="Dev Answers"
+          width={23}
+          height={23}
+        />
+        <p className="h2-bold text-dark-100 dark:text-light-900 max-sm:hidden">
+          Dev <span className="text-primary-500">Answers</span>
+        </p>
+      </Link>
+      GlobalSearch
+      <div className="flex-between gap-5">
+        Theme
+        <SignedIn>
+          <UserButton
+            afterSignOutUrl="/"
+            appearance={{
+              elements: { avatarBox: "h-10 w-10" },
+              variables: { colorPrimary: "#ff7000" },
+            }}
+          />
+        </SignedIn>
+        MobileNavbar
+      </div>
+    </nav>
+  );
+}
