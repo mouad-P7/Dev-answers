c8d438ff67eacad1bb256294c52988108d15a236
mouad-p7 <mouadananouch7@gmail.com>
2023-09-29 16:54:41 +0100

    Implement shadcn library and add <Menubar/>.

---
+++ src/components/shared/Theme.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
+
+export default function Theme() {
+  return (
+    <Menubar className="border-none">
+      <MenubarMenu>
+        <MenubarTrigger>File</MenubarTrigger>
+        <MenubarContent>
+          <MenubarItem>
+            New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+          </MenubarItem>
+          <MenubarItem>New Window</MenubarItem>
+          <MenubarSeparator />
+          <MenubarItem>Share</MenubarItem>
+          <MenubarSeparator />
+          <MenubarItem>Print</MenubarItem>
+        </MenubarContent>
+      </MenubarMenu>
+    </Menubar>
+  );
+}
