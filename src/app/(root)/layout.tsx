83dc930283ee1785f19ef089800d3cc1e5524d9d
mouad-p7 <mouadananouch7@gmail.com>
2023-09-30 01:52:30 +0100

    Add <LeftSidebar/>.

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "@/components/shared/Navbar";
+import LeftSidebar from "@/components/shared/LeftSidebar";
 
 export default function RootLayout({
   children,
@@ -10,7 +11,7 @@ export default function RootLayout({
     <div className="background-light850_dark100 relative">
       <Navbar />
       <div className="flex">
-        LeftSideBar
+        <LeftSidebar />
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
           <div className="mx-auto w-full max-w-5xl">{children}</div>
         </section>
