fafa1b92aa664117b408d9c44df3e3d5ff803c33
mouad-p7 <mouadananouch7@gmail.com>
2023-10-04 21:18:40 +0100

    Create User Model.

---
+++ src/database/user.model.ts
@@ -0,0 +1,34 @@
+import { Schema, model, models } from "mongoose";
+
+export interface UserType extends Document {
+  clerkId: string;
+  fullName: string;
+  userName: string;
+  email: string;
+  password?: string;
+  bio?: string;
+  picture: string;
+  location?: string;
+  portfolioWebsite?: string;
+  reputation?: number;
+  saved: Schema.Types.ObjectId[];
+  joinedAt: Date;
+}
+
+const UserSchema = new Schema({
+  clerkId: { type: String, required: true },
+  fullName: { type: String, required: true },
+  userName: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String },
+  bio: { type: String },
+  picture: { type: String, required: true },
+  location: { type: String },
+  portfolioWebsite: { type: String },
+  reputation: { type: Number, default: 0 },
+  saved: [{ type: Schema.Types.ObjectId, ref: "Question" }],
+  joinedAt: { type: Date, default: Date.now },
+});
+
+const User = models.User || model("User", UserSchema);
+export default User;
