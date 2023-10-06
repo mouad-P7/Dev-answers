1f4de2e890a14724acb4d740f82a36ec4236fc25
mouad-p7 <mouadananouch7@gmail.com>
2023-10-06 23:52:31 +0100

    Implement clerk webhooks and create some user actions.

--- src/lib/actions/user.action.ts
+++ src/lib/actions/user.action.ts
@@ -1,7 +1,64 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
 import User from "@/database/user.model";
+import Question from "@/database/question.model";
 import { connectToDatabase } from "@/lib/mongoose";
+import {
+  createUserParams,
+  updateUserParams,
+  deleteUserParams,
+} from "@/types/actions";
+
+export async function deleteUser(params: deleteUserParams) {
+  try {
+    connectToDatabase();
+    const { clerkId } = params;
+    const user = await User.findOneAndDelete({ clerkId });
+    if (!user) {
+      throw new Error("User not found");
+    }
+    // Delete user from database
+    // and questions, answers, comments, etc.
+
+    // get user question ids
+    // const userQuestionIds = await Question.find({ author: user._id }).distinct("_id");
+
+    // delete user questions
+    await Question.deleteMany({ author: user._id });
+
+    // TODO: delete user answers, comments, etc.
+
+    const deletedUser = await User.findByIdAndDelete(user._id);
+    return deletedUser;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+export async function updateUser(params: updateUserParams) {
+  try {
+    connectToDatabase();
+    const { clerkId, updateData, path } = params;
+    await User.findOneAndUpdate({ clerkId }, updateData, { new: true });
+    revalidatePath(path);
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+export async function createUser(userData: createUserParams) {
+  try {
+    connectToDatabase();
+    const newUser = await User.create(userData);
+    return newUser;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
 
 export async function getUserById(params: any) {
   try {
--- src/types/actions.d.ts
+++ src/types/actions.d.ts
@@ -1,6 +1,24 @@
 import { Schema } from "mongoose";
 import { UserType } from "@/database/user.model";
 
+export interface deleteUserParams {
+  clerkId: string;
+}
+
+export interface updateUserParams {
+  clerkId: string;
+  updateData: Partial<UserType>;
+  path: string;
+}
+
+export interface createUserParams {
+  clerkId: string;
+  name: string;
+  userName: string;
+  email: string;
+  picture: string;
+}
+
 export interface postQuestionParams {
   title: string;
   explanation: string;
