59488679cbb45797e25cb9ff46ca712d40a05e92
mouad-p7 <mouadananouch7@gmail.com>
2023-11-06 15:16:21 +0100

    Add url query feature to <Filter/>.

--- src/components/shared/Filter.tsx
+++ src/components/shared/Filter.tsx
@@ -1,3 +1,8 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { useRouter, useSearchParams, usePathname } from "next/navigation";
+import { formUrlQuery } from "@/lib/query";
 import {
   Select,
   SelectContent,
@@ -18,9 +23,23 @@ export default function Filter({
   containerClasses,
   otherClasses,
 }: FilterProps) {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const query = searchParams.get("filter");
+  const [filter, setFilter] = useState(query || "old_users");
+
+  useEffect(() => {
+    const newUrl = formUrlQuery(searchParams.toString(), "filter", filter);
+    router.push(newUrl, { scroll: false });
+  }, [filter, pathname, router, searchParams, query]);
+
   return (
     <div className={containerClasses}>
-      <Select>
+      <Select
+        onValueChange={(value) => setFilter(value || "old_users")}
+        defaultValue={filter}
+      >
         <SelectTrigger className={otherClasses}>
           <SelectValue placeholder="Select a Filter" />
         </SelectTrigger>
